a53b5dacac92e8e455686af34327f9f6a5679049
Mikaila Williams <babysis@ymail.com>
2021-08-14 12:55:22 -0400

    new styles

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('alright')
+localStorage.setItem('myCar', 'VW');
+localStorage.setItem('myPet', 'EllaBella');
+localStorage.setItem('petAge', 1);
+localStorage.setItem('food', 'Pasta');
+localStorage.setItem('myPet', 'Mia');
+
+var myCar = localStorage.getItem('myCar')
+var myPet = localStorage.getItem('myPet')
+var petAge = localStorage.getItem('petAge')
+
+localStorage.removeItem('myCar')
